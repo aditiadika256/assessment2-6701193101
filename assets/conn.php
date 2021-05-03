ed84ab74fa33f961139b7ed9450c4df95c7bc2ee
Aditia Dika <75602174+aditiadika256@users.noreply.github.com>
2021-05-03 20:41:05 +0700

    Update conn.php

--- assets/conn.php
+++ assets/conn.php
@@ -1,9 +1,9 @@
 <?php 
-$host = "localhost";
+$host = "ec2-52-87-107-83.compute-1.amazonaws.com";
 $port = "5432";
-$dbname = "assessment2";
-$user = "postgres";
-$password = "lse34233"; 
+$dbname = "dchnm0uhvk8aiu";
+$user = "xfdrzkvbmvrlsk";
+$password = "2dd6e2fb165814190e5d04e373b7fe8c76e79373153dd070eb3a799d16c8c289"; 
 $connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password} ";
 $dbconn = pg_connect($connection_string);
 ?>
